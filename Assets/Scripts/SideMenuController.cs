2e6b8cf4aea7e07dfddf00710413f71db6692ad2
Jonathan Kiessling <joki@mail.upb.de>
2017-11-02 23:09:01 +0100

    o push meta and temp files

---
+++ Assets/Scripts/SideMenuController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SideMenuController : MonoBehaviour {
+
+	public Text health;
+	public Text money;
+	public GameObject Tower1;
+
+	private bool isDragging = false;
+	private GameObject draggedTower;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isDragging) {
+			var mousePos = Input.mousePosition;
+			mousePos.z = 2.0f;       // we want 2m away from the camera position
+			var objectPos = Camera.current.ScreenToWorldPoint(mousePos);
+			draggedTower.transform.position = objectPos;
+		}
+	}
+
+	public void UpdateHealth(string amount) {
+		health.text = amount;
+	}
+
+	public void BuildTower(string tower) {
+		Debug.Log ("building tower " + tower);
+
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = 2.0f;       // we want 2m away from the camera position
+		Vector3 objectPos = Camera.current.ScreenToWorldPoint(mousePos);
+		GameObject draggedTower = Instantiate(Tower1, objectPos, Quaternion.identity);
+
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	private GameObject enemyContainer;
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		enemyContainer = GameObject.FindWithTag("EnemyContainer");
+
+		InvokeRepeating ("SpawnMob", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void SpawnMob() {
+
+		GameObject mob = Instantiate (enemy, transform.position, Quaternion.identity);
+		if (enemyContainer != null) {
+			mob.transform.SetParent (enemyContainer.transform);
+		}
+
+	}
+}
