006be44f2f830e6561362c75cf326626c24db912
jvkies <jvkies@users.noreply.github.com>
2017-12-02 01:07:36 +0100

    add the new files i forgot in the last commit

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+	private Image blendImage;
+
+	public float fadeTime = 1f;
+
+	// Use this for initialization
+	void Start () {
+		blendImage = GetComponent<Image> ();
+		StartCoroutine (FadeingIn ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator FadeingIn() {
+		float t = 1f;
+
+		while (t > 0) {
+			t -= Time.deltaTime * (1/fadeTime);
+			blendImage.color = new Color (1, 1, 1, t);
+			yield return 0;
+		}
+		gameObject.SetActive (false);
+
+	}
+
+}
---
+++ Assets/Scripts/SetVersion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetVersion : MonoBehaviour {
+
+	public TextAsset version;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = version.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
