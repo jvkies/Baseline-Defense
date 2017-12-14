f1bf900a4b3d61478c1b1e099af9c73e9c59083d
jvkies <jvkies@users.noreply.github.com>
2017-12-14 19:10:10 +0100

    o tweaked SoulRepeller

---
+++ Assets/Scripts/SoulRepeller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulRepeller : MonoBehaviour {
+
+	private PointEffector2D pe2d;
+
+	public float intervall = 1f;
+
+	// Use this for initialization
+	void Start () {
+		pe2d = GetComponent<PointEffector2D> ();
+		StartCoroutine (OnOffSwitch ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	private IEnumerator OnOffSwitch() {
+		while (true) {
+			yield return new WaitForSeconds (intervall);
+			pe2d.enabled = !pe2d.enabled;
+		}
+	}
+}
