9bfd2cf5084af43c57776c12f58f61398a905650
Friedemann Kiessling <friedemann.kiessling@quinscape.de>
2017-12-15 18:42:25 -0500

    Fix Bullet AoE Calculation

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -15,7 +15,6 @@ public class Bullet : MonoBehaviour {
 
 	void Start() {
 		enemyContainer = GameObject.FindWithTag ("EnemyContainer").transform;
-
 	}
 
 	// Update is called once per frame
@@ -28,26 +27,39 @@ public class Bullet : MonoBehaviour {
 		Vector3 dir = target.position - transform.position;
 		float distanceThisFrame = velocity * Time.deltaTime;
 
-		if (dir.magnitude <= distanceThisFrame) {
-			if (aoeRange != 0) {
-				for (int i = 0; i < enemyContainer.childCount; i++) {
-					if ((enemyContainer.GetChild (i).transform.position - transform.position).magnitude <= aoeRange)
-						enemyContainer.GetChild (i).GetComponent<MobController> ().TakeDamage (damage);
-				}
-			}
-			HitTarget ();
-			return;
-		}
-
 		transform.Translate (dir.normalized * distanceThisFrame, Space.World);
 	}
+		 
+	void OnTriggerEnter2D(Collider2D collider2d) {
+		// TODO: Jona baut hier den BlastEffect ein
+//		GameObject be = Instantiate (blastEffect,transform.position, Quaternion.identity);
+//		be.transform.localScale = Vector3.one * BlastRadius;
+//		Destroy (be, 0.2f);
 
-	private void HitTarget() {
-		target.GetComponent<MobController> ().TakeDamage (damage);
+		// Calculates Targets for AoE Damage and applies Damage
+		if (aoeRange != 0) 
+		{
+			Collider2D[] colliders = Physics2D.OverlapCircleAll (transform.position, aoeRange);
+			Debug.Log ("enemys hit: " + colliders.Length);
+			for (int i = 0; i < colliders.Length; i++) 
+			{
+				MobController enemyC = colliders [i].GetComponent<MobController> ();
+				if (enemyC != null) 
+				{
+					enemyC.TakeDamage (damage);
+				}
+			}
+		} 
+		// Applies Damage to Non AoE Target
+		else 
+		{
+			target.GetComponent<MobController> ().TakeDamage (damage);
+		}
+			
 		target.GetComponent<MobController> ().mobData.incomingDmg -= damage;
 		Destroy (gameObject);
 	}
-
+		
 	public void Seek(Transform _target) {
 		target = _target;
 		target.GetComponent<MobController> ().mobData.incomingDmg += damage;
